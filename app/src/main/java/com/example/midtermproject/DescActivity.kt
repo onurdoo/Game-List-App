f210b42ab15b7ff118af75fbbe65855675ccc8ff
onurdoo <onurbahar71@gmail.com>
2022-12-07 03:13:31 +0300

    1.4.0

---
+++ app/src/main/java/com/example/midtermproject/DescActivity.kt
@@ -0,0 +1,11 @@
+package com.example.midtermproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DescActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_desc)
+    }
+}
--- app/src/main/java/com/example/midtermproject/MainActivity.kt
+++ app/src/main/java/com/example/midtermproject/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.midtermproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,25 +11,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var aa = Game("divinity","98","rpg","metacritic",null)
-        var bb = Game("Life is Strange","100","lesbian love","metacritic",null)
-        var cc = Game("gtav","96","action","metacritic",null)
-        var dd = Game("gtav","96","action","metacritic",null)
-        var ee = Game("gtav","96","action","metacritic",null)
-        var ff = Game("gtav","96","action","metacritic",null)
-        var gg = Game("gtav","96","action","metacritic",null)
 
-        val games = arrayOf(
-            aa,bb,cc,dd,ee,ff,gg
-        )
+        val gameFragment = GameFragment()
+        val favFragment = FavoritesFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.mainFrame, gameFragment)
+            commit()
+        }
+        var button1 :Button = findViewById(R.id.gameButton)
+        var button2 :Button = findViewById(R.id.favButton)
 
-        var customAdapter = CustomAdapter(games)
+        button1.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.mainFrame, gameFragment)
+                addToBackStack(null)
+                commit()
 
-        var myRecyclerView = findViewById<RecyclerView>(R.id.gamesList)
-        myRecyclerView.layoutManager = LinearLayoutManager(this)
-        myRecyclerView.adapter = customAdapter
-       // myRecyclerView.setOnClickListener { myRecyclerView.setBackgroundColor(getColor(R.color.aa))}
+            }
+        }
+        button2.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.mainFrame, favFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
 
+
+
+        /*fun gamesTab(view: View){
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            fragmentTransaction.replace(R.id.mainFrame,gameFragment).commit()
+        }
+
+        fun favoritesTab(view: View){
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            fragmentTransaction.replace(R.id.mainFrame,favFragment).commit()
+        }*/
 
     }
 
